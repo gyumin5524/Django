8482ac2b3ab25aab91caeadfb872a3c080692cf1
gyumin <rbals5524@gmail.com>
2025-02-03 15:49:23 +0900

    모델 수정

--- post/models.py
+++ post/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=500, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
 
 # Create your models here.
